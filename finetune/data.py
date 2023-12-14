3029b3cb6b1a1a727cb2f16b82a49b356f0cc5e3
Eduardo Dadalto <edadaltocg@gmail.com>
2023-12-14 09:17:21 -0600

    simple dataloader

---
+++ finetune/data.py
@@ -0,0 +1,34 @@
+import torch
+import os
+import numpy as np
+from pathlib import Path
+
+
+def poorsmans_dataloader(dataset, batch_size, block_size, device, device_type):
+    root = os.environ.get("DATA_DIR", "data")
+    data_dir = os.path.join(root, dataset)
+    train_data = np.memmap(
+        os.path.join(data_dir, "train.bin"), dtype=np.uint16, mode="r"
+    )
+    val_data = np.memmap(os.path.join(data_dir, "val.bin"), dtype=np.uint16, mode="r")
+
+    def get_batch(split):
+        data = train_data if split == "train" else val_data
+        ix = torch.randint(len(data) - block_size, (batch_size,))
+        x = torch.stack(
+            [torch.from_numpy((data[i : i + block_size]).astype(np.int64)) for i in ix]
+        )
+        y = torch.stack(
+            [
+                torch.from_numpy((data[i + 1 : i + 1 + block_size]).astype(np.int64))
+                for i in ix
+            ]
+        )
+        if device_type == "cuda":
+            # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
+            x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(
+                device, non_blocking=True
+            )
+        else:
+            x, y = x.to(device), y.to(device)
+        return x, y
